9d74106eecb1a3d6a83023974f175882203d783d
Erik Knudsen <31932266+erik1066@users.noreply.github.com>
2019-03-23 07:56:52 -0400

    Added Operator enum

--- src/Models/Operators.cs
+++ src/Models/Operators.cs
@@ -1,7 +1,41 @@
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace Foundation.RulesEngine.Models
 {
-    public enum Operators
+    [DataContract]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum Operator
     {
-        
+        Undefined = 0,
+        [EnumMember(Value = "$all")]
+        All,
+        [EnumMember(Value = "$exists")]
+        Exists,
+        [EnumMember(Value = "$eq")]
+        Equal,
+        [EnumMember(Value = "$ne")]
+        NotEqual,
+        [EnumMember(Value = "$gt")]
+        GreaterThan,
+        [EnumMember(Value = "$gte")]
+        GreaterThanOrEqualTo,
+        [EnumMember(Value = "$lt")]
+        LessThan,
+        [EnumMember(Value = "$lte")]
+        LessThanOrEqualTo,
+        [EnumMember(Value = "$in")]
+        In,
+        [EnumMember(Value = "$nin")]
+        NotIn,
+        [EnumMember(Value = "$regex")]
+        RegularExpression,
+        [EnumMember(Value = "$mod")]
+        Modulus,
+        [EnumMember(Value = "$size")]
+        Size,
+        [EnumMember(Value = "$type")]
+        Type
     }
 }
